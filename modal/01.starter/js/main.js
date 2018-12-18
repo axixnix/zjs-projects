e0694a279a3b361dddc765d3011a0ccaa49880d9
axixnix <henryirons@yahoo.com>
2018-12-18 22:42:43 +0100

    modal completed but clicking outside the modal does not close it

--- modal/01.starter/js/main.js
+++ modal/01.starter/js/main.js
@@ -1,2 +1,15 @@
 // START EDITING YOUR JAVASCRIPT HERE
 // ===============
+const toggleButton = document.querySelector('.jsModalToggle')
+
+const closeButton = document.querySelector('.jsModalClose')
+
+toggleButton.addEventListener('click', _ => {
+  document.body.classList.add('modal-is-open')
+  console.log('open')
+})
+
+closeButton.addEventListener('click', _ => {
+  document.body.classList.remove('modal-is-open')
+  console.log('close')
+})
