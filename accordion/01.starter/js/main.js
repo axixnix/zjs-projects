4dc85b581f3167e9e266ecee0d963e92077107fd
axixnix <henryirons@yahoo.com>
2018-12-22 22:29:18 +0100

    added JS to all accordions so that a click opens or closes any accordion where the click occured

--- accordion/01.starter/js/main.js
+++ accordion/01.starter/js/main.js
@@ -1,2 +1,12 @@
 // START EDITING YOUR JAVASCRIPT HERE
 // ===============
+const accordions = Array.from(document.querySelectorAll('.accordion'))
+
+
+accordions.forEach(accordion =>{
+    const accordionHeader = accordion.querySelector('.accordion__header')
+
+    accordionHeader.addEventListener('click',e =>{
+        accordion.classList.toggle('is-open')
+    })
+})
